c46e613563edb1336d91ac6f64533ba283ecbd87
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2022-10-11 17:04:45 +0300

    Added a folder: Problem solving

---
+++ Solving_the_problem/Program.cs
@@ -0,0 +1,24 @@
+﻿// 
+string[] massStr = new string[5] {"hello", "world", "Big", "Dog", "777"};
+string[] newStr = new string[massStr.Length];
+void NewArrayStr(string[] massStr, string[] newStr)
+{
+    int count = 0;
+    for (int i = 0; i < massStr.Length; i++)
+    {
+    if(massStr[i].Length <= 3)
+        {
+        newStr[count] = massStr[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+NewArrayStr(massStr, newStr);
+PrintArray(newStr);
